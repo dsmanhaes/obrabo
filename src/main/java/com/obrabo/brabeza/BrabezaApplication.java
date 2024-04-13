889bf7125f76a8a86cf700bc1febd3b9952152eb
Diogo Manhaes <dsmanhaes@gmail.com>
2024-04-12 22:21:44 -0300

    initial commit

---
+++ src/main/java/com/obrabo/brabeza/BrabezaApplication.java
@@ -0,0 +1,13 @@
+package com.obrabo.brabeza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrabezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrabezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obrabo/brabeza/BrabezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obrabo.brabeza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrabezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
